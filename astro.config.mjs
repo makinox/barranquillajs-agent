78092c25dc2af62ffb834f84f6196e428aa68748
Jesus Bossa <x@jesusbossa.dev>
2024-12-09 11:56:38 -0500

    feat: add vercel deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server"
+  output: "server",
+  adapter: vercel({})
 });
